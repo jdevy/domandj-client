33f83298587b90f60513707360cc2e4e0e2456ec
DEVY Jerome <jdevy.ext@orange.com>
2018-08-29 10:58:43 +0200

    test server.js avec proxy

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
+
 var express = require('express');
-var app = express();
-var path = require('path');
+var httpProxy = require('http-proxy');
+var bodyParser = require('body-parser');
+
+var apiForwardingUrl = 'https://domandj-server.herokuapp.com:443/';
 
 const port = process.env.PORT || '5000';
 
-app.set('port', port);
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-    res.set('Content-Type', 'text/html')
-        .sendFile(path.join(__dirname + '/dist/index.html'));
-})
 
-app.listen(app.get('port'), function() {
-    console.log('Console listening on port : ' + app.get('port'));
+// Solution for forwarding from http to https taken from:
+// http://stackoverflow.com/questions/15801014/how-to-use-node-http-proxy-for-http-to-https-routing
+var proxyOptions = {
+    changeOrigin: true
+};
+
+httpProxy.prototype.onError = function (err) {
+    console.log(err);
+};
+
+var apiProxy = httpProxy.createProxyServer(proxyOptions);
+
+console.log('Forwarding API requests to ' + apiForwardingUrl);
+
+// Node express server setup.
+var server = express();
+server.set('port', port);
+server.use(express.static(__dirname + '/dist'));
+
+server.get("*", function(req, res) {
+    apiProxy.web(req, res, {target: apiForwardingUrl});
 });
+   
+
+// Start Server.
+server.listen(server.get('port'), function() {
+    console.log('Express server listening on port ' + server.get('port'));
+});
+
+
+//--- version initiale ----
+
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+
+// const port = process.env.PORT || '5000';
+
+// app.set('port', port);
+// app.use(express.static(__dirname + '/dist'));
+// app.get('/*', function(req, res) {
+//     res.set('Content-Type', 'text/html')
+//         .sendFile(path.join(__dirname + '/dist/index.html'));
+// })
+
+// app.listen(app.get('port'), function() {
+//     console.log('Console listening on port : ' + app.get('port'));
+// });
