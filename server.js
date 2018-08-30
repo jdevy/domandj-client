91cfdab4790762cda4cea36bbcd8cb7542652a1f
DEVY Jerome <jdevy.ext@orange.com>
2018-08-30 17:46:43 +0200

    modifie server.js test

--- server.js
+++ server.js
@@ -27,13 +27,12 @@ server.set('port', port);
 server.use(express.static(__dirname + '/dist'));
 
 function forceHttps(req, res, next){
-    if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+    if (!req.secure) {
       res.redirect('https://' + req.headers.host + req.url);
     }else {
       return next();
     }
-  };
-
+};
 
 server.all("*", function(req, res) {
     apiProxy.web(req, res, {target: apiForwardingUrl});
