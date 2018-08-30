997a8f0789f8428175cce643e4295765c069b281
DEVY Jerome <jdevy.ext@orange.com>
2018-08-30 16:40:50 +0200

    modifie server.js retour

--- server.js
+++ server.js
@@ -26,9 +26,18 @@ var server = express();
 server.set('port', port);
 server.use(express.static(__dirname + '/dist'));
 
+function forceHttps(req, res, next){
+    if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+      res.redirect('https://' + req.headers.host + req.url);
+    }else {
+      return next();
+    }
+  };
+
+
 server.all("*", function(req, res) {
     apiProxy.web(req, res, {target: apiForwardingUrl});
-});
+}).all("*", forceHttps);
    
 
 // Start Server.
